ae209a917c5e712f240deacf410e917706af313a
KatOli4ka <katoli4ka87@mail.ru>
2022-10-29 13:49:34 +0500

    Ввод остальных переменных по заданию

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,13 @@ public class Main {
         System.out.println("Значение переменной temperature с типом int равно " + temperature);
         short money=10000;
         System.out.println("Значение переменной money с типом short равно " + money);
+        long distanceToMoon=384467;
+        System.out.println("Значение переменной distanceToMoon с типом long равно " + distanceToMoon+ " км");
+        double distanceToMars=55.76;
+        System.out.println("Значение переменной distanceToMars с типом double равно " + distanceToMars + " млн. км");
+        float mosquitoWeight=3.2F;
+        System.out.println("Значение переменной mosquitoWeight с типом float равно " + mosquitoWeight + " г");
+
+
     }
 }
