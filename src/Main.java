a0c609207ec619a6153bd155bc1f15a6837f92cc
KatOli4ka <katoli4ka87@mail.ru>
2022-10-29 13:34:35 +0500

    Ввод переменной int

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         byte ball=5;
         System.out.println("Значение переменной ball с типом byte равно " + ball);
+        int temperature=-28;
+        System.out.println("Значение переменной temperature с типом int равно " + temperature);
     }
 }
